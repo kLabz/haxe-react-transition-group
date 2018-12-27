45dac3fcfb76da49e63ab1e526775b019ccb04c6
k <k@klabz.org>
2018-12-27 12:33:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Haxe externs for `react-transition-group`
+
+See the package's [documentation](http://reactcommunity.org/react-transition-group/).
+
+Note: this needs a recent version of `react-types` and `react-next` (instead of `react`).
---
+++ src/react/transition/TransitionMacro.hx
@@ -0,0 +1,21 @@
+package react.transition;
+
+import haxe.macro.Expr;
+import react.macro.PropsValidator;
+
+class TransitionMacro {
+	public static inline var TRANSITION_PROPS_VALIDATOR_KEY = 'react.transition.Transition';
+
+	public static function registerValidator() {
+		PropsValidator.register(TRANSITION_PROPS_VALIDATOR_KEY, validator);
+	}
+
+	public static function validator(name:String, expr:Expr):Null<Expr> {
+		// Props that are reserved keywords in Haxe
+		if (name == 'in') return macro @:pos(expr.pos) (${expr}:Bool);
+
+		// Accept all other props (will be passed as ChildProps)
+		return expr;
+	}
+}
+
